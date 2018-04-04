2da50cbc7a5ae7b4197ebd06eb983c0220c2bd07
abrocks <abrocks@mit.edu>
2018-04-04 10:31:39 -0400

    first commit

--- exercise.js
+++ exercise.js
@@ -2,26 +2,34 @@ var exercise = {};
 
 // calculate distance between points
 exercise.distance = function(p1,p2){
-    //-------------------
-    //---- Your Code ----
-    //-------------------    
-    return 'Error: 1st function not implemented';    
+    return Math.sqrt(
+        Math.pow((p2.x-p1.x), 2)+Math.pow((p2.y-p1.y), 2)
+    ); 
+    //return 'Error: 1st function not implemented';
+    /*
+    var dx = p2.x-p1.x;
+    var dy = p2.y-p1.y
+    return Math.sqrt(dx*dx+ dy*dy); 
+    */     
 };
 
 // calculate distance between new point and existing
 exercise.distances = function(newPoint,data){
-    //-------------------
-    //---- Your Code ----
-    //-------------------
-    return 'Error: 2nd function not implemented';
-};
+    data.forEach(function(element){
+        element.distance = exercise.distance(newPoint, element); 
+        //console.log(element); 
+    });
+    return data;  
+    //return 'Error: 2nd function not implemented';
+}; 
 
 // find k closest points
 exercise.findClosest = function(k,data){
-    //-------------------
-    //---- Your Code ----
-    //------------------- 
-    return 'Error: 3rd function not implemented';    
+    data.sort(function(a,b){
+        return a.distance - b.distance;
+    })
+    //return 'Error: 3rd function not implemented'; 
+    return data.slice(0,k);    
 };
 
 module.exports = exercise;
